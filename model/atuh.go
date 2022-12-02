fa3c784f654fa2fb8544f58085713e587ed3290d
rnwxyz <jamesstrom78@gmail.com>
2022-12-02 22:17:57 +0800

    fix: body response in admin login

--- model/atuh.go
+++ model/atuh.go
@@ -1,6 +1,21 @@
 package model
 
+import "time"
+
 type Token struct {
 	AccessToken  string `json:"access_token"`
 	RefreshToken string `json:"refresh_token" validate:"required"`
+}
+type AdminToken struct {
+	Access  Access  `json:"access"`
+	Refresh Refresh `json:"refresh"`
+}
+
+type Access struct {
+	AccessAccessToken string    `json:"token"`
+	ExpiredAt         time.Time `json:"expired_at"`
+}
+type Refresh struct {
+	RefreshToken string    `json:"token"`
+	ExpiredAt    time.Time `json:"expired_at"`
 }
